f54eb3e2dc84abf4b5c5a935c7ae1c00391c4419
Moling <1970115881@qq.com>
2022-02-22 22:36:23 +0800

    Revert "new"
    This reverts commit b46d873fdc2f65ec49c36a82e9ff936a4170fa04.

--- JavaScript/Tools/weDay.js
+++
@@ -1,9 +0,0 @@
-/* 
- *@author:Mol
- *@create:12点38分
- */
-var weDay = new Date(),
-    year = weDay.getFullYear(),
-    month = weDay.getMonth(),
-    day = weDay.getDate(),
-    hour = weDay.getHours();
--- README.md
+++
@@ -1,2 +0,0 @@
-# Scripts
-When you came, i was
