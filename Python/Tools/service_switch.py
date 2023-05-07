ca9ca419e004d8dcbd5eed94ba8f80865419d8a5
Moling <1970115881@qq.com>
2023-05-07 11:07:01 +0800

    Update service_switch.py

--- Python/Tools/service_switch.py
+++ Python/Tools/service_switch.py
@@ -4,20 +4,21 @@ import sys
 
 
 # 检查服务是否存活,并返回状态
-def check_plan_service_live(service_name: str = 'MongoDB') -> bool:
+def check_plan_service_live(service_name:str) -> bool:
     # 获取服务列表
     service_list = os.popen('net start').read()
     # 判断服务是否存在
     if service_name == "MySQL":
         service_name = os.popen('sc queryex type= service state= all | findstr "SERVICE_NAME:" | findstr "MySQL"').read().split()[1]
+        
     if service_name in service_list:
-        return True
+        return True,service_name
     else:
-        return False
+        return False,service_name
 
 
 #  检测服务status,并提示用户是否启动或关闭服务
-def check_service_state(status: bool):
+def check_service_state(status: bool,service_name:str):
     if status:
         print(f'{service_name}服务已启动')
         if input(f'是否关闭{service_name}服务？(y/n)') == 'y':
@@ -57,8 +58,7 @@ def check_service():
     user_input = input("输入需要检查的服务序号(默认为MongoDB)：") or 1
     # 判断用户输入是否为数字,并且在switch字典的键值范围内
     if user_input.isdigit() and int(user_input) in switch:
-        global service_name
-        service_name = switch[int(user_input)]
+        return switch[int(user_input)]
     else:
         print('输入错误')
         sys.exit()
@@ -66,6 +66,7 @@ def check_service():
 
 if __name__ == "__main__":
     # check_admin()
-    check_service()
-    alive_service = check_plan_service_live()
-    check_service_state(alive_service)
+    switch_item=check_service()
+    alive_service,service_name= check_plan_service_live(switch_item)
+    print('当前为',service_name)
+    check_service_state(alive_service,service_name)
