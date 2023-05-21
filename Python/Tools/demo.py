0c9730e634013dac2d5a5d62e90c8dd8d5efd134
Moling <1970115881@qq.com>
2023-05-22 00:36:25 +0800

    Update demo.py

--- Python/Tools/demo.py
+++ Python/Tools/demo.py
@@ -1,17 +1,24 @@
 import os
 import sys
-# 检测当前是什么系统什么环境
-def check_system():
-    if sys.platform == 'win32':
-        return 'Windows'
-    elif sys.platform == 'linux':
-        return 'Linux'
-    else:
-        print('不支持的操作系统')
-        sys.exit()
-# 检查当前系统是否安装了Python
+# # 检测当前是什么系统什么环境
+# def check_system():
+#     if sys.platform == 'win32':
+#         return 'Windows'
+#     elif sys.platform == 'linux':
+#         return 'Linux'
+#     else:
+#         print('不支持的操作系统')
+#         sys.exit()
+# # 检查当前系统是否安装了Python
 
-if __name__ == '__main__':
-    text=check_system()
-    print(text)
-    print('脚本运行结束')
+# if __name__ == '__main__':
+#     text=check_system()
+#     print(text)
+#     print('脚本运行结束')
+
+# 打开一个cmd窗口
+# 设置打开的窗口大小，标题，颜色
+os.system('mode con cols=80 lines=20')
+os.system('title Windows服务开关')
+os.system('color 0a')
+os.system('cmd')
